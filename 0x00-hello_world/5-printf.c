e749a73ba57f289be3c7d20ce5f2b4be43b965c2
cakoth77 <christineberny77@gmail.com>
2023-02-16 00:04:47 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints "with pproper grammar, but the outcome is a piece of art," followed by a newline
+ * main - prints "with pproper grammar, but the outcome is
+ * a piece of art," followed by a newline
  * Return: Always 0.
  *
  */
