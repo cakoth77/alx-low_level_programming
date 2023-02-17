8e0b59aaddd7958ff16c26f35d5cc98061481c90
cakoth77 <christineberny77@gmail.com>
2023-02-17 00:10:48 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints the alphabet in reverse
+ * Return: 0.
+ */
+int main(void)
+{
+char c;
+c = 'z';
+while
+(c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
