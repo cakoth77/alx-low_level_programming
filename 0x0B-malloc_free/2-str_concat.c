0c90f11ce4ca48ac5ee72a0349ceb11369fd9ac0
cakoth77 <christineberny77@gmail.com>
2023-03-13 23:28:47 -0700

    2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,7 +25,7 @@ return (i);
  */
 char *str_concat(char *s1, char *s2)
 {
-char *s;
+char *dst;
 unsigned int i, j, size;
 if (s1 == NULL)
 s1 = "";
@@ -33,16 +33,16 @@ if (s2 == NULL)
 s2 = "";
 size = (_strlen(s1) + _strlen(s2) + 1);
 dst = (char *) malloc(size *sizeof(char));
-if (s == 0)
+if (dst == 0)
 {
 return (NULL);
 }
 for (i = 0; *(s1 + i) != '\0'; i++)
-*(s + 1) = *(s1 + i);
+*(dst + 1) = *(s1 + i);
 for (j = 0; *(s2 + j) != '\0'; j++)
 {
-*(s + i) = *(s2 + j);
+*(dst + i) = *(s2 + j);
 i++;
 }
-return (s);
+return (dst);
 }
