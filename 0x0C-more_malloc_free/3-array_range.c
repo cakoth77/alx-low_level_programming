de43320cb2920a1cc2beb280e0028ac229e4219a
cakoth77 <christineberny77@gmail.com>
2023-03-16 02:34:22 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,13 +7,18 @@
  */
 int *array_range(int min, int max)
 {
-int *ar, i = 0;
+int range, i;
+int *ar;
+range = 0;
 if (min > max)
 return (0);
+range = ((max + 1) - min);
 ar = malloc((max - min + 1) * sizeof(int));
 if (!ar)
 return (0);
-while (i <= max - min)
-ar[i++] = min++;
+for (i = 0; i < range; i++)
+{
+*(ar + i) = min + i;
+}
 return (ar);
 }
