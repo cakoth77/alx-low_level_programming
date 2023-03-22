14270cd19d098e88ecd821635f9f0a2bacc3d820
cakoth77 <christineberny77@gmail.com>
2023-03-22 05:59:45 -0700

    3 main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,29 +8,30 @@
  * @argv: array of pointers to the arguments
  * Return: Always 0.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int num1, num2;
+int num1, num2, result;
 char *op;
+int (*func)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
 num1 = atoi(argv[1]);
-op = argv[2];
-num2 = atoi(argv[1]);
-if (get_op_func(op) == NULL || op[1] != '\0')
+num2 = atoi(argv[2]);
+func = get_op_func(argv[2]);
+if (!func)
 {
 printf("Error\n");
 exit(99);
 }
-if ((*op == '/' && num2 == 0) ||
-(*op == '1' && num2 == 0))
+if ((op == '/' || op == '1') && num2 == 0)
 {
 printf("Error\n");
 exit(100);
 }
-printf("%d\n", get_op_func(op)(num1, num2));
+result = func(num1, num2);
+printf("%d\n", result);
 return (0);
 }
