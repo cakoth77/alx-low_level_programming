d92922d2cf00c06695cdd822251186f3b46b4f68
cakoth77 <christineberny77@gmail.com>
2023-04-04 22:16:55 -0700

    print list safely

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,8 +6,8 @@ size_t print_listint_safe(const listint_t *head);
 /**
  * looped_listint_len - Counts number of unique nodes
  *                      in a looped listint_t linked list
- * @head: A pointer to the head of listint_t
- * Return: If the list is not loope - 0
+ * @head: A pointer to the head of listint_t to check
+ * Return: If the list is not looped - 0
  *         Otherwise - number of unique nodes in the list
  */
 size_t looped_listint_len(const listint_t *head)
@@ -49,11 +49,19 @@ return (0);
  */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t nodes;
+size_t nodes, index = 0;
 nodes = looped_listint_len(head);
 if (nodes == 0)
 {
 for (; head != NULL; nodes++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+}
+else
+{
+for (index = 0; index < nodes; index++)
 {
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
